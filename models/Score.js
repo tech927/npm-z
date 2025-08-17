ec70a87b2127a4f3b2c20abf1be894c4e0324bb3
tech927 <onnui688@gmail.com>
2025-08-16 22:19:31 -0300

    Create Score.js

---
+++ models/Score.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const scoreSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Score', scoreSchema);
