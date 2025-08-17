6cd73a6668ca0639179ecdeedda2a520b5134e34
tech927 <onnui688@gmail.com>
2025-08-16 22:19:02 -0300

    Create Question.js

---
+++ models/Question.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  options: {
+    type: [String],
+    required: true,
+    validate: {
+      validator: function(v) {
+        return v.length === 4;
+      },
+      message: props => `Must have exactly 4 options, got ${props.value.length}`
+    }
+  },
+  correctAnswer: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 3
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Question', questionSchema);
